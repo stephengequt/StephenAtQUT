fa0de2a85b7513a640f993dd3480caa9bcf19f52
Stephen Ge <stephen.ge.qut@gmail.com>
2018-04-21 01:08:35 +1000

    UpdateSpark
    Spark

--- SparkFractal/src/main/java/Fractal.java
+++ SparkFractal/src/main/java/Fractal.java
@@ -1,5 +1,12 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Fractal {
@@ -24,7 +31,30 @@ public class Fractal {
 		double[][] VV = new double[num_q][networkDiameter];
 		double[] UU = new double[networkDiameter];
 
+
+
+
+
 		try {
+//
+//			Logger.getLogger("org").setLevel(Level.ERROR);
+//			SparkConf conf = new SparkConf().setAppName("Calculation").setMaster("local[*]");
+//			JavaSparkContext sc = new JavaSparkContext(conf);
+//
+//			int NUM_SAMPLES = 100;
+//			List<Integer> l = new ArrayList<>(NUM_SAMPLES);
+//			for (int i = 0; i < NUM_SAMPLES; i++) {
+//				l.add(i);
+//			}
+//
+//			long count = sc.parallelize(l).filter(i -> {
+//				double x = Math.random();
+//				double y = Math.random();
+//				return x*x + y*y < 1;
+//			}).count();
+//			System.out.println("Pi is roughly " + 4.0 * count / NUM_SAMPLES);
+
+
 			for (int i = 0; i < centerNode.length; i++) {   // for sample nodes, calculate UU and VV
 				if (i % 100 == 0) {
 					System.out.printf("Progess: %.3f%%\n", (((double) i) / centerNode.length) * 100);
--- SparkFractal/src/main/java/fractalUWG.java
+++ SparkFractal/src/main/java/fractalUWG.java
@@ -1,22 +1,32 @@
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class fractalUWG {
 	public static void main(String arg[]) {
 		
 //		String fileName = "Input/11_sparse.dat" ;
-		String fileName = "Input/sparse.dat" ;
+		String fileName = "Input/9_sparse.dat" ;
 		Input sparse = new Input();
 		
 		long startTime=System.currentTimeMillis();
 		
-		sparse.E = sparse.getVE("Input/E.dat");   // Read the number of edges in the network
-		sparse.V = sparse.getVE("Input/V.dat");   // Read the number of nodes in the network
+		sparse.E = sparse.getVE("Input/9_E.dat");   // Read the number of edges in the network
+		sparse.V = sparse.getVE("Input/9_V.dat");   // Read the number of nodes in the network
 		sparse.configuration(fileName, sparse.V, sparse.E);   // Convert the standard sparse matrix to compressed sparse matrix
 
 		long inputProcessTime = System.currentTimeMillis();
 
 		System.out.println("Input Process Time:"+(inputProcessTime-startTime)+"ms");
 
-		Fractal.calculate(sparse.V, sparse.offset, sparse.column);
+
+
+//		Fractal.calculate(sparse.V, sparse.offset, sparse.column);
+		distributedFractal.distributedCalculate(sparse.V, sparse.offset, sparse.column);
 
 		long calculationTime=System.currentTimeMillis(); // Get start time
 		System.out.println("Calculation time:"+(calculationTime-inputProcessTime)+"ms");
